0035b242cb8d014c388b5b4c8dc81a75a9b66aeb
Aadil Tamboli <aadiltamboli07@gmail.com>
2026-03-03 14:52:15 +0530

    add the ChatStore.js file

--- Frontend/src/Store/useAuthStore.js
+++ Frontend/src/Store/useAuthStore.js
@@ -8,10 +8,13 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
   isSigningUp: false,
+  isLoggingIn: false,
 
   checkAuth: async () => {
     try {
-      const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/check`)
+      const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/auth/check`, {
+        withCredentials: true
+      })
       set({ authUser: res.data })
     } catch (error) {
       console.log("error in the useAuth", error);
@@ -25,7 +28,9 @@ export const useAuthStore = create((set) => ({
   signup: async (data) => {
     set({ isSigningUp: true })
     try {
-      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/signup`,data)
+      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/signup`, data, {
+        withCredentials: true
+      })
       set({ authUser: res.data });
       toast.success("Account created successfully !");
 
@@ -36,5 +41,33 @@ export const useAuthStore = create((set) => ({
       set({ isSigningUp: false })
     }
   }
+  ,
+  login: async (data) => {
+    set({ isLogingIn: true })
+    try {
+      const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/login`, data, {
+        withCredentials: true
+      })
+      set({ authUser: res.data });
+      toast.success("LoggedIn SuccessFully successfully ");
 
-}));
+    } catch (error) {
+      console.log("erro in the signup function ")
+      toast.error(error.message)
+    } finally {
+      set({ isLogingIn: false })
+    }
+  }
+  ,
+  logout: async () => {
+    try {
+      await axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/logout`, {
+        withCredentials: true
+      })
+      set({ authUser: null })
+      toast.success("Logged out Successfully")
+    } catch (error) {
+      console.log("error in the logout frontend")
+    }
+  }
+}
